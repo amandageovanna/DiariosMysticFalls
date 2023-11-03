d192ffff4a8bb0f4a44f29cd98fbf4793b723a45
amandageovanna <amanda.sousa@sptech.school>
2023-11-03 20:52:52 -0300

    criei os sliders, sessao de valores e sobre mim

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaImg()
+}, 5000)
+
+function proximaImg(){
+    cont++
+
+    if(cont > 5){
+        cont = 1 
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
+});
+
 function playGif(image) {
     image.src = 'imagens-serie/gifElenaVampira.gif'
 }
