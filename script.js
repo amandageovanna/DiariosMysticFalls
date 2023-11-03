1f3d4d1b474a3cddeb64a004f8c9e5a1e2dc3f36
amandageovanna <amanda.sousa@sptech.school>
2023-11-03 00:50:45 -0300

    estruturei as colunas da sessao de personagens, adicionei textos

---
+++ script.js
@@ -0,0 +1,47 @@
+function playGif(image) {
+    image.src = 'imagens-serie/gifElenaVampira.gif'
+}
+
+function stopGif(image) {
+    image.src = 'imagens-serie/vampEspecie.png'
+}
+
+function playGif1(image) {
+    image.src = 'imagens-serie/gifLobisomem.gif'
+}
+
+function stopGif1(image) {
+    image.src = 'imagens-serie/lobEspecie.png'
+}
+
+function playGif2(image) {
+    image.src = 'imagens-serie/elijahGif.gif'
+}
+
+function stopGif2(image) {
+    image.src = 'imagens-serie/elijahEspecie.png'
+}
+
+function playGif3(image) {
+    image.src = 'imagens-serie/gifKlaus.gif'
+}
+
+function stopGif3(image) {
+    image.src = 'imagens-serie/klausEspecie.png'
+}
+
+function playGif4(image) {
+    image.src = 'imagens-serie/duplicatasGif.gif'
+}
+
+function stopGif4(image) {
+    image.src = 'imagens-serie/doppelSessao.png'
+}
+
+function playGif5(image) {
+    image.src = 'imagens-serie/gifBruxa.gif'
+}
+function stopGif5(image) {
+    image.src = 'imagens-serie/bruxaEspecie.png'
+}
+
