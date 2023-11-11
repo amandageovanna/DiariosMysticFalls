fe7c7fd548b08cf3113abcff88e47fc438445a28
amandageovanna <amanda.sousa@sptech.school>
2023-11-10 22:37:28 -0300

    organização inicial das pastas, mudei as cores das páginas

---
+++ PaginaInicial/script.js
@@ -0,0 +1,47 @@
+function playGif(image) {
+    image.src = 'Imagens/gifElenaVampira.gif'
+}
+
+function stopGif(image) {
+    image.src = 'Imagens/vampEspecie.png'
+}
+
+function playGif1(image) {
+    image.src = 'Imagens/gifLobisomem.gif'
+}
+
+function stopGif1(image) {
+    image.src = 'Imagens/lobEspecie.png'
+}
+
+function playGif2(image) {
+    image.src = 'Imagens/elijahGif.gif'
+}
+
+function stopGif2(image) {
+    image.src = 'Imagens/elijahEspecie.png'
+}
+
+function playGif3(image) {
+    image.src = 'Imagens/gifKlaus.gif'
+}
+
+function stopGif3(image) {
+    image.src = 'Imagens/klausEspecie.png'
+}
+
+function playGif4(image) {
+    image.src = 'Imagens/duplicatasGif.gif'
+}
+
+function stopGif4(image) {
+    image.src = 'Imagens/doppelSessao.png'
+}
+
+function playGif5(image) {
+    image.src = 'Imagens/gifBruxa.gif'
+}
+function stopGif5(image) {
+    image.src = 'Imagens/bruxaEspecie.png'
+}
+
