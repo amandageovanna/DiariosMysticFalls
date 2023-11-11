489b78cb500571dd0cc3837708c7447fb2fe807f
amandageovanna <amanda.sousa@sptech.school>
2023-11-11 02:09:55 -0300

    inseri função para ser chamada quando clicada nos botões, alterei a navbar da página inicial, adicionei API de video na pagina inicial

--- PaginaInicial/script.js
+++ PaginaInicial/script.js
@@ -1,3 +1,8 @@
+function telaLogin() {
+    window.location.href = "../Login/index.html"
+}
+
+// funções para chamar o gif //
 function playGif(image) {
     image.src = 'Imagens/gifElenaVampira.gif'
 }
@@ -44,4 +49,53 @@ function playGif5(image) {
 function stopGif5(image) {
     image.src = 'Imagens/bruxaEspecie.png'
 }
+
+// // API do Youtube 
+
+// var player; // Variável global para armazenar a instância do player
+
+// function onYouTubeIframeAPIReady() {  
+//   player = new YT.Player('player', {
+//     height: '530',
+//     width: '900',
+//     videoId: 'xyOOJII6a2Q', //Video ID vídeo
+//     playerVars: {
+//       'autoplay': 0, // 0 desativa a reprodução automática
+//       'controls': 1, // 1 exibe os controles do player
+//       'showinfo': 0, // 0 oculta informações do vídeo (título, etc.)
+//       'rel': 0, // 0 desativa vídeos relacionados no final
+//     },
+//     events: {       
+        
+//     }
+//   });
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const VIMEO_URL = 'https://vimeo.com/883470644'; // Defina a URL do vídeo do Vimeo
+    const VIDEO_ID = VIMEO_URL.split('/').pop(); // 
+
+    // Configurações do player
+    const options = {
+        id: VIDEO_ID,
+        width: 940,
+        autoplay: false,
+        portrait: 0, // Define para 0 para ocultar a foto do perfil
+        title: 0,    // Define para 0 para ocultar o título do vídeo
+    };
+
+    
+    const playerElement = document.getElementById('vimeoPlayer'); // Obter o elemento do player pelo ID
+
+   // Verificar se o elemento foi encontrado                
+    if (playerElement) {
+        const player = new Vimeo.Player(playerElement, options);  
+
+        player.on('play', () => {
+            console.log('O vídeo foi reproduzido!');
+        });
+    } else {
+        console.error('Elemento com o ID "vimeoPlayer" não encontrado.');
+    }
+});
 
