e2d632e5632a1ce0d3ea4805d38baea1b76ced99
amandageovanna <amanda.sousa@sptech.school>
2023-11-13 01:10:31 -0300

    tentativa de usar javascript para a sessao de personagens

--- PaginaInicial/script.js
+++ PaginaInicial/script.js
@@ -1,3 +1,44 @@
+
+// função para mudar as informações com base no personagem clicado
+function mudarInformacoes(personagemId) {
+    // pega as referências dos elementos caixaDireita2 e caixaEsquerda2
+    var caixaDireita2 = document.querySelector('.caixaDireita2');
+    var caixaEsquerda2 = document.querySelector('.caixaEsquerda2');
+
+    // vetores com informações dos personagens
+    var titulos = ["STEFAN SALVATORE"];
+    var citacoes = ["A vida não se resume a momentos finais e sim aos momentos que levaram a eles"];
+    var imgSrcs = ["imagens-serie/stefanSalvatore.png"];
+    var infoBibliografica = [["Nascimento", "1 de Novembro, 1846"]];
+    // achar a posição do personagem no vetor
+    var posicao = titulos.indexOf(personagemId);
+
+    // atualiza os elementos HTML com as informações do personagem
+    if (posicao !== -1) {
+        caixaDireita2.innerHTML = "<h6 class='TituloSessaoPersonagem'>" +
+            titulos[posicao] + "</h6> <div class='caixaCitacao'> <p>" +
+            citacoes[posicao] + "</p> </div>";
+
+        caixaEsquerda2.innerHTML = "<img class='imgSessaoPersonagem' src='" +
+            imgSrcs[posicao] +
+            "'><div><p class='TituloSessaoPersEsq'>INFORMAÇÃO BIBLIOGRÁFICA</p></div><div class='coluna'>" +
+            "<div class='linhas'><div class='info'><div class='esquerda'>" + infoBibliografica[posicao][0] + ":</div><div class='direita'>" + infoBibliografica[posicao][1] + "</div></div></div>" +
+            "</div>";
+    }
+}
+      
+
+//  associar manipuladores de eventos a cada imagem em um loop.
+var imagens = document.querySelectorAll('.tamanhoPers');
+
+for (var i = 0; i < imagens.length; i++) {
+    imagens[i].addEventListener('click', function (event) {
+        var personagemId = event.target.id; // chama o ID da imagem clicada
+        mudarInformacoes(personagemId);
+    });
+}
+
+
 function telaLogin() {
     window.location.href = "../Login/index.html"
 }
@@ -50,28 +91,9 @@ function stopGif5(image) {
     image.src = 'Imagens/bruxaEspecie.png'
 }
 
-// // API do Youtube 
+// // API do Vimeo 
 
-// var player; // Variável global para armazenar a instância do player
-
-// function onYouTubeIframeAPIReady() {  
-//   player = new YT.Player('player', {
-//     height: '530',
-//     width: '900',
-//     videoId: 'xyOOJII6a2Q', //Video ID vídeo
-//     playerVars: {
-//       'autoplay': 0, // 0 desativa a reprodução automática
-//       'controls': 1, // 1 exibe os controles do player
-//       'showinfo': 0, // 0 oculta informações do vídeo (título, etc.)
-//       'rel': 0, // 0 desativa vídeos relacionados no final
-//     },
-//     events: {       
-        
-//     }
-//   });
-// }
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const VIMEO_URL = 'https://vimeo.com/883470644'; // Defina a URL do vídeo do Vimeo
     const VIDEO_ID = VIMEO_URL.split('/').pop(); // 
 
@@ -84,12 +106,12 @@ document.addEventListener('DOMContentLoaded', function() {
         title: 0,    // Define para 0 para ocultar o título do vídeo
     };
 
-    
+
     const playerElement = document.getElementById('vimeoPlayer'); // Obter o elemento do player pelo ID
 
-   // Verificar se o elemento foi encontrado                
+    // Verificar se o elemento foi encontrado                
     if (playerElement) {
-        const player = new Vimeo.Player(playerElement, options);  
+        const player = new Vimeo.Player(playerElement, options);
 
         player.on('play', () => {
             console.log('O vídeo foi reproduzido!');
@@ -98,4 +120,3 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error('Elemento com o ID "vimeoPlayer" não encontrado.');
     }
 });
-
