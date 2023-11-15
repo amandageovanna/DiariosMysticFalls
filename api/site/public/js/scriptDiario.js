24f31f67fe147cf9cddeb27cd7a502706b99c92c
amandageovanna <amanda.sousa@sptech.school>
2023-11-15 16:10:51 -0300

    organizei os arquivos e adaptei para o formato da API

--- api/site/public/js/scriptDiario.js
+++
@@ -1,9 +0,0 @@
-function telaMetricas() {
-    window.location.href = "dashboard.html"}
-
-    function telaQuiz() {
-    window.location.href = "quiz.html"}
-
-    function telaSair() {
-    window.location.href = "index.html"}
-  
